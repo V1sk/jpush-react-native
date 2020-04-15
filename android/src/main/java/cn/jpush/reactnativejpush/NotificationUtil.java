7284b8d7baa9e0e2dbcd31414d0c1d4f0f728b39
cjw <938548157@qq.com>
2020-04-15 10:42:48 +0800

    防止链式调用

--- android/src/main/java/cn/jpush/reactnativejpush/NotificationUtil.java
+++ android/src/main/java/cn/jpush/reactnativejpush/NotificationUtil.java
@@ -8,7 +8,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.os.Build;
-import android.support.v4.app.NotificationCompat;
+
+import androidx.core.app.NotificationCompat;
 
 /**
  * Created by chenjianwei on 2018/2/25.
